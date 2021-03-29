0896b144e55d2d25f7e1d41d3d7725a3bda38e11
Vishal Kumar <leader.vishalkumar@gmail.com>
2021-03-29 16:44:10 +0530

    Added: API
    App properly work with API and callbacks.

--- app.js
+++ app.js
@@ -6,10 +6,30 @@
 
 var btnTranslate= document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function errorHandler(error){
+    alert("Something went wrong. Try again later..!");
+}
+
+function addQueryToURL(inputText){
+    return serverURL + "?"+ "text="+ inputText
+}
 
 function clickHandler() {
-    console.log("clicked");
-    console.log(txtInput.value);
-}
+    
+    var text = txtInput.value;
+
+        fetch(addQueryToURL(text))
+        .then(response => response.json() )
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
+        .catch(errorHandler)
+        }  
+
 
 btnTranslate.addEventListener("click", clickHandler);
